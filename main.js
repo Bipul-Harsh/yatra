493abb46fb668035b7e1bbfeb6409377026c48a2
Bipul-Harsh <bipulharsh123@gmail.com>
2021-05-16 21:38:32 +0530

    demo check

---
+++ main.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            navItems: [
+                {name: "Flights", alt:"flight icon", icon: "/assets/images/flight.png"},
+                {name: "Hotels", alt:"hotel icon", icon: "/assets/images/hotel.png"},
+                {name: "Packages", alt:"packages icon", icon: "/assets/images/"}
+            ],
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+})
