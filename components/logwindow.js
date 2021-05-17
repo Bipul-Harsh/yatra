c6fc29417001b94e71e24cdd2358daa37c525703
Bipul-Harsh <bipulharsh123@gmail.com>
2021-05-18 00:35:58 +0530

    login work

---
+++ components/logwindow.js
@@ -0,0 +1,16 @@
+app.component("logwindow", {
+    props: {
+
+    },
+    template:
+    /*html*/
+    `<div>Hello World</div>`,
+    data(){
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+})
--- main.js
+++ main.js
@@ -6,15 +6,20 @@ const app = Vue.createApp({
                 {name: "Hotels", alt:"hotel icon", icon: "/assets/images/hotel.svg"},
                 {name: "Packages", alt:"packages icon", icon: "/assets/images/package.svg"}
             ],
-            checkoutItems: [],
-            authUser: [
-                {userName: "bipul"}
+            checkoutItems: [
+
             ],
-            currUser: 0
+            authUser: [
+                // {userName: "bipul"}
+            ],
+            currUser: 0,
+            logwindowShow: false,
         }
     },
     methods: {
-
+        handlelogwindow(){
+            this.logwindowShow = this.logwindowShow?false:true;
+        }
     },
     computed: {
 
