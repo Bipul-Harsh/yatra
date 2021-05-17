8d7d1beb47651d499ebc1ab126a1a0a3d76665ed
Bipul-Harsh <bipulharsh123@gmail.com>
2021-05-17 22:12:44 +0530

    png to svg

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const app = Vue.createApp({
     data(){
         return {
             navItems: [
-                {name: "Flights", alt:"flight icon", icon: "/assets/images/flight.png"},
-                {name: "Hotels", alt:"hotel icon", icon: "/assets/images/hotel.png"},
-                {name: "Packages", alt:"packages icon", icon: "/assets/images/package.png"}
+                {name: "Flights", alt:"flight icon", icon: "/assets/images/flight.svg"},
+                {name: "Hotels", alt:"hotel icon", icon: "/assets/images/hotel.svg"},
+                {name: "Packages", alt:"packages icon", icon: "/assets/images/package.svg"}
             ],
             checkoutItems: [],
             authUser: [
